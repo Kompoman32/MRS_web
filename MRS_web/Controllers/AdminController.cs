ae59c421f93167c8a71e9aafe9f1339841087d0d
Слава Липин <Kompoman32@yandex.ru>
2018-06-14 02:25:19 +0500

    +AddMeter
    using jquery (yeaaaah (guck this shit))
    
    TODO:
    1)DocumentCheck
    2)LoadToDataBase

--- MRS_web/Controllers/AdminController.cs
+++ MRS_web/Controllers/AdminController.cs
@@ -6,7 +6,9 @@ using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Html;
+using System.Web.UI;
 using System.Web.UI.WebControls;
+using Microsoft.Ajax.Utilities;
 using MRS_web.Models.EDM;
 using WebGrease.Css.Extensions;
 
@@ -48,13 +50,151 @@ namespace MRS_web.Controllers
             return View();
         }
 
-        public ActionResult Addmeter()
+        #region AddMeter
+
+        private void AddMeterInitFields()
         {
+            Session["AddMetUser"] = Request.Form["User"] != null && Request.Form["User"] != "" ? Request.Form["User"] : Session["AddMetUser"];
+            Session["AddMetName"] = Request.Form["Name"] != null && Request.Form["Name"] != "" ? Request.Form["Name"] : Session["AddMetName"];
+            Session["AddMetDescription"] = Request.Form["Description"] != null && Request.Form["Description"] != "" ? Request.Form["Description"] : Session["AddMetDescription"];
+            Session["AddMetCapacityBefComma"] = Request.Form["CapacityBefComma"] != null && Request.Form["CapacityBefComma"] != "" ? Request.Form["CapacityBefComma"] : Session["AddMetCapacityBefComma"];
+            Session["AddMetCapacityAftComma"] = Request.Form["CapacityAftComma"] != null && Request.Form["CapacityAftComma"] != "" ? Request.Form["CapacityAftComma"] : Session["AddMetCapacityAftComma"];
+            Session["AddMetProductionId"] = Request.Form["ProductionId"] != null && Request.Form["ProductionId"] != "" ? Request.Form["ProductionId"] : Session["AddMetProductionId"];
+            Session["AddMetProductionDate"] = Request.Form["ProductionDate"] != null && Request.Form["ProductionDate"] != "" ? Request.Form["ProductionDate"] : Session["AddMetProductionDate"];
+            Session["AddMetExpirationDate"] = Request.Form["ExpirationDate"] != null && Request.Form["ExpirationDate"] != "" ? Request.Form["ExpirationDate"] : Session["AddMetExpirationDate"];
+            Session["AddMetTypeId"] = Request.Form["TypeId"] != null && Request.Form["TypeId"] != "" ? Request.Form["TypeId"] : Session["AddMetTypeId"];
+            Session["AddMetReadings"] = Request.Form["Readings"] != null && Request.Form["Readings"] != "" ? Request.Form["Readings"] : Session["AddMetReadings"];
+            Session["AddMetParameters"] = Request.Form["MeterParameters"] != null && Request.Form["MeterParameters"] != "" ? Request.Form["MeterParameters"] : Session["AddMetParameters"];
+
+
+            ViewData["Users"] = _DataManager.UserRepo.Users();
+            ViewData["User"] = Session["AddMetUser"];
+            ViewData["Name"] = Session["AddMetName"];
+            ViewData["Description"] = Session["AddMetDescription"];
+            ViewData["Capacity"] = Session["AddMetCapacity"];
+            ViewData["CapacityBefComma"] = Session["AddMetCapacityBefComma"];
+            ViewData["CapacityAftComma"] = Session["AddMetCapacityAftComma"];
+            ViewData["ProductionId"] = Session["AddMetProductionId"];
+            ViewData["ProductionDate"] = Session["AddMetProductionDate"];
+            ViewData["ExpirationDate"] = Session["AddMetExpirationDate"];
+            ViewData["Types"] = _DataManager.TypeRepo.Types();
+            ViewData["TypeId"] = Session["AddMetTypeId"];
+            ViewData["Tariffs"] = _DataManager.TarRepo.Tariffs();
+            ViewData["Readings"] = Session["AddMetReadings"];
+            ViewData["Documents"] = Session["AddMetDocuments"]??new List<Document>();
+            ViewData["MeterParameters"] = Session["AddMetrParameters"];
+            ViewData["StockParameters"] = ViewData["MeterParameters"] != null
+                ? from par in _DataManager.ParRepo.Parametrs()
+                where !(ViewData["MeterParameters"] as IEnumerable<Parametr>).Select(x => x.Id).Contains(par.Id)
+                select par
+                : _DataManager.ParRepo.Parametrs();
+        }
+
+        [HttpGet]
+        public ActionResult AddMeter()
+        {
+            AddMeterInitFields();
             return View();
         }
 
+        [HttpPost]
+        public ActionResult AddMeter(string User, string Name, string Description, int? TypeId, int? CapacityBefComma, int? CapacityAftComma, long? ProductionId, DateTime? ProductionDate, DateTime? ExpirationDate, int? TariffId, string[] Reading, string[] meterParameters)
+        {
+            AddMeterInitFields();
+
+            if (CheclFields(User, Name, Description, TypeId, CapacityBefComma, CapacityAftComma, ProductionId, ProductionDate, 
+                ExpirationDate, TariffId,Reading, meterParameters))
+                return HttpNotFound();
+            
+            return View();
+            
+        }
+
+        private bool CheclFields(string User, string Name, string Description, int? TypeId, int? CapacityBefComma, int? CapacityAftComma,
+                                 long? ProductionId, DateTime? ProductionDate, DateTime? ExpirationDate, int? TariffId, string[] Reading, string[] meterParameters)
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+                ModelState.AddModelError("Name","Заполните поле");
+
+            if (string.IsNullOrWhiteSpace(Description))
+                ModelState.AddModelError("Description", "Заполните поле");
+
+            if (CapacityBefComma==null)
+                ModelState.AddModelError("CapacityBefComma", "Заполните поле");
+
+            if (CapacityAftComma == null)
+                ModelState.AddModelError("CapacityAftComma", "Заполните поле");
+
+            if (ProductionId == null)
+                ModelState.AddModelError("ProductionId", "Заполните поле");
+
+            if(ProductionId != null && (from m in _DataManager.MetRepo.Meters() where m.ProductionId == ProductionId select m).Any())
+                ModelState.AddModelError("ProductionId", "Счётчик с таким номером уже существует");
+
+            if (ProductionDate == null)
+                ModelState.AddModelError("ProductionDate", "Заполните поле верной датой");
+
+            if (ProductionDate != null && ProductionDate > DateTime.Now)
+                ModelState.AddModelError("ProductionDate", "Дата производства, не может быть больше текущей");
+
+            if (ExpirationDate == null)
+                ModelState.AddModelError("ProductionDate", "Заполните поле верной датой");
+
+            if (ExpirationDate != null && ExpirationDate < DateTime.Now)
+                ModelState.AddModelError("ProductionDate", "Дата проверки, не может быть меньше текущей");
+
+            if(TariffId!=null)
+            for (int i = 0; i < TariffId; i++)
+            {
+                if(Reading[i].IsNullOrWhiteSpace())
+                    ModelState.AddModelError("Reading"+i, "Заполните поле");
+            }
+            
+            return ModelState.IsValid;
+        }
+
+        public ActionResult Tariffes(string Count)
+        {
+            if (Count != null)
+            {
+                return PartialView("PartialViews/Tarifes",int.Parse(Count));
+            }
+            return HttpNotFound();
+        }
+
+
+        public ActionResult NewDocumentSave(string Title, string Discription, DateTime? Date)
+        {
+            //TODO Проверку
+            var t = Session["AddMetDocuments"] as IEnumerable<Document>;
+            Document doc = new Document(){Discription = Discription,SigningDate = (DateTime)Date, Title = Title};
+
+            var documents = t != null?new List<Document>(t) :new List<Document>();
+            documents.Add(doc);
+            Session["AddMetDocuments"] = documents;
+
+            return PartialView("PartialViews/Documents",documents);
+        }
+
+        public ActionResult RemoveDocuments(string[] documentId)
+        {
+            List<Document> t = Session["AddMetDocuments"] as List<Document>;
+            if (t==null) t = new List<Document>();
+
+            foreach (var s in documentId)
+            {
+                t.RemoveAt(int.Parse(s));
+            }
+
+            Session["AddMetDocuments"] = t;
+
+            return PartialView("PartialViews/Documents",t);
+        }
+
+        #endregion AddMeter
+
         #region Extend
-        
+
         [HttpGet]
         public ActionResult ExtendMeter()
         {
